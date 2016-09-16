4bdd393d6ad53e4266faf74a0a79f972b40ae493
Alan Bandeira <alan@iMac-de-Alan.local>
2016-09-16 16:35:28 -0300

    bg_1021

--- bg_1021.py
+++ bg_1021.py
@@ -1,36 +1,34 @@
 import math
 
-data = [float(x) for x in input().split(".")]
 
-h = int(data[0] / 100)
-ft = int((data[0] % 100) / 50)
-tt = int(((data[0] % 100) % 50) / 20)
-t = int((((data[0] % 100) % 50) % 20) / 10)
-f = int(((((data[0] % 100) % 50) % 20) % 10) / 5)
-tw = int((((((data[0] % 100) % 50) % 20) % 10) % 5) / 2)
+data = float(input()) * 100
 
-data[1] = data[1] / 100 + int((((((data[0] % 100) % 50) % 20) % 10) % 5) % 2)
+h = int(data / 10000)
+ft = int((data % 10000) / 5000)
+tt = int(((data % 10000) % 5000) / 2000)
+t = int((((data % 10000) % 5000) % 2000) / 1000)
+f = int(((((data % 10000) % 5000) % 2000) % 1000) / 500)
+tw = int((((((data % 10000) % 5000) % 2000) % 1000) % 500) / 200)
 
-m_o = int(data[1] / 1.00)
-m_ft = int((data[1] % 1.00) / 0.50)
-m_tf = int(((data[1] % 1.00) % 0.50) / 0.25)
-m_t = int((((data[1] % 1.00) % 0.50) % 0.25) / 0.10)
-m_f = int(((((data[1] % 1.00) % 0.50) % 0.25) % 0.10) / 0.05)
-#m_oo = math.ceil((((((data[1] % 1.00) % 0.50) % 0.25) % 10) % 0.05) / 0.01)
-m_oo = int((((((data[1] % 1.00) % 0.50) % 0.25) % 10) % 0.05) / 0.01)
-print (((((data[1] % 1.00) % 0.50) % 0.25) % 10))
+m_o = int(((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) / 100)
+m_ft = int((((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) % 100) / 50)
+m_tf = int(((((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) % 100) % 50) / 25)
+m_t = int((((((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) % 100) % 50) % 25) / 10)
+m_f = int(((((((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) % 100) % 50) % 25) % 10) / 5)
+m_oo = int((((((((((((data % 10000) % 5000) % 2000) % 1000) % 500) % 200) % 100) % 50) % 25) % 10) % 5) / 1)
+
 
 print ("NOTAS:\n"
-	"%i nota(s) de 100\n"% (h) +
-	"%i nota(s) de 50\n" % (ft) +
-	"%i nota(s) de 20\n" % (tt) +
-	"%i nota(s) de 10\n" % (t) +
-	"%i nota(s) de 5\n" % (f) +
-	"%i nota(s) de 2\n" % (tw) + 
+	"%i nota(s) de R$ 100.00\n"% (h) +
+	"%i nota(s) de R$ 50.00\n" % (ft) +
+	"%i nota(s) de R$ 20.00\n" % (tt) +
+	"%i nota(s) de R$ 10.00\n" % (t) +
+	"%i nota(s) de R$ 5.00\n" % (f) +
+	"%i nota(s) de R$ 2.00\n" % (tw) + 
 	"MOEDAS:\n" + 
-	"%i moeda(s) de 1.00\n" % (m_o) + 
-	"%i moeda(s) de 0.50\n" % (m_ft) +
-	"%i moeda(s) de 0.25\n" % (m_tf) +
-	"%i moeda(s) de 0.10\n" % (m_t) +
-	"%i moeda(s) de 0.05\n" % (m_f) +
-	"%i moeda(s) de 0.01" % (m_oo))
+	"%i moeda(s) de R$ 1.00\n" % (m_o) + 
+	"%i moeda(s) de R$ 0.50\n" % (m_ft) +
+	"%i moeda(s) de R$ 0.25\n" % (m_tf) +
+	"%i moeda(s) de R$ 0.10\n" % (m_t) +
+	"%i moeda(s) de R$ 0.05\n" % (m_f) +
+	"%i moeda(s) de R$ 0.01" % (m_oo))
---
+++ bg_1022.py
@@ -0,0 +1,7 @@
+n = [int(x) for x in input().split()]
+
+
+if n[1] > n[2] and n[3] > n[0] and (n[2] + n[3]) > (n[0] + n[1]) and n[2] >= 0 and n[3] >= 0 and n[0] % 2 is 0:
+	print ("Valores aceitos")
+else:
+	print ("Valores nao aceitos")
